84e3e2cbf37dcfdf2f934b05cf341983bc83d340
natanael986 <juniornatan986@gmail.com>
2022-08-29 20:17:09 -0300

    commit

--- Palindromo/palinScript.js
+++ Palindromo/palinScript.js
@@ -1,19 +1,16 @@
 const button = document.querySelector('#button');
 const resultText = document.querySelector('#result');
 
-function checkPalindromo(str){
-    for(let i = 0 ; i < str.length / 2; i++)
-        if (str[i] != str[str.length - i - 1])
-            return false;
-    return true;
+function invertText(text) {
+  const changedText = text.split('').reverse().join('');
+  return text === changedText ? true : false;
 }
-const result  = invertText(textValue);
 
 button.addEventListener('click', () => {
-    const textValue = document.querySelector('#text').value;
-    if (checkPalindromo(str) = false)
-        result
-            ? resultText.innerHTML = '<div class="alert alert-danger d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg><div>O texto não é um palídromo</div></div>'
-    : resultText.innerHTML = '<div class="alert alert-success d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg><div>O texto é um palídromo</div></div>';
-    
+  const textValue = document.querySelector('#text').value;
+  const result  = invertText(textValue);
+
+  result
+    ? resultText.innerHTML = 'O texto é um palídromo'
+  : resultText.innerHTML = 'O texto não é um palídromo';
 });
