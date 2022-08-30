4a93b5d21bb65a3eaca32de3257f9379ae0828f4
natanael986 <juniornatan986@gmail.com>
2022-08-29 21:37:31 -0300

    commit

--- Palindromo/palinScript.js
+++ Palindromo/palinScript.js
@@ -1,33 +1,20 @@
-const button = document.querySelector('#buton');
-const resultText = document.querySelector('#result');
-/*
-function invertText(text) {
-    const changedText = text.split('').reverse().join('');
-    return text === changedText ? true : false;
+var el = document.getElementById("btVerificar");
+el.addEventListener("click", isPalindromo, false);
+
+function isPalindromo() {
+    let texto = document.getElementById('txtbox').value;
+    let textoSemEspaco = texto.replace(" ", "").toLowerCase();
+    let textInvertido = inverter(textoSemEspaco);
+
+    if (textoSemEspaco == textInvertido)
+        alert(texto + " é um palindromo")
+    alert(texto + " não é um palindromo")
 }
 
-button.addEventListener('click', () => {
-    const textValue = document.querySelector('#text').value;
-    const result = invertText(textValue);
-
-    result
-        ? resultText.innerHTML = 'O texto é um palídromo'
-        : resultText.innerHTML = 'O texto não é um palídromo';
-});*/
-
-function isPalidrome1(button) {
-    let arrayString = [...button];
-    let numOfLetters = button.length - 1;
-    let isPalidrome = '<div class="alert alert-success d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg><div>O texto é um palídromo</div></div>';
-
-    for (let i = numOfLetters, j = 0; i >= 0; i--, j++) {
-        let firstStringLetterOfIteration = arrayString[j];
-        let lastStringLetterOfIteration = arrayString[i];
-
-        if (lastStringLetterOfIteration !== firstStringLetterOfIteration) {
-            isPalidrome = '<div class="alert alert-primary d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg><div>O texto não é um palídromo</div></div>';
-            break;
-        }
+function inverter(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
     }
-    return isPalidrome;
+    return newString;
 }
