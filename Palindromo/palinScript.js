99414525df7b6530525650c879c9ea128fb8a135
natanael986 <juniornatan986@gmail.com>
2022-09-04 15:21:26 -0300

    Update palinScript.js

--- Palindromo/palinScript.js
+++ Palindromo/palinScript.js
@@ -1,16 +1,6 @@
 let el = document.getElementById("btVerificar");
 el.addEventListener("click", isPalindromo, false);
 
-function isPalindromo() {
-    let texto = document.getElementById('txtbox').value;
-    let textoSemEspaco = texto.replace(" ", "").toLowerCase();
-    let textInvertido = inverter(textoSemEspaco);
-
-    if (textoSemEspaco == textInvertido)
-        alert(texto + " é um palindromo")
-    alert(texto + " não é um palindromo")
-}
-
 function inverter(str) {
     var newString = "";
     for (var i = str.length - 1; i >= 0; i--) {
@@ -18,3 +8,16 @@ function inverter(str) {
     }
     return newString;
 }
+
+function isPalindromo() {
+    let texto = document.getElementById('txtbox').value;
+    let textoSemEspaco = texto.replace(" ", "").toLowerCase();
+    let textInvertido = inverter(textoSemEspaco);
+
+    if (textoSemEspaco == textInvertido){
+        alert(texto + " é um palindromo")
+
+    }else
+        alert(texto + " não é um palindromo")
+}
+
