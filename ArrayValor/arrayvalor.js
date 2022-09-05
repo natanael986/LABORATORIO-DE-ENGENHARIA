711630e9bf30e63e2374e9eb25c7899e5512dc96
natanael986 <juniornatan986@gmail.com>
2022-09-05 20:17:18 -0300

    array

---
+++ ArrayValor/arrayvalor.js
@@ -0,0 +1,21 @@
+let btnAdicionar = document.getElementById("btnAdicionar");
+btnAdicionar.addEventListener("click", function () {
+    
+    document.getElementById("txtarray").innerHTML = "TESTE";
+
+    // let elementos = [];
+    // let valor = document.getElementById("valor").value;
+    // elementos.push(valor.toString());
+
+    // let elementoOL = document.createElement("OL");
+    // elementos.forEach(element => {
+    //     let elementLI = document.createElement("LI");
+    //     let textNode = document.createTextNode(element);
+    //     elementLI.appendChild(textNode);
+
+    //     elementoOL.appendChild(elementLI);
+    // });
+
+    // document.body.appendChild(elementoOL);
+
+});
