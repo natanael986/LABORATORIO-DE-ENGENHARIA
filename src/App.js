41fad38324473cdf6973bca83a8ed67e3a542026
natanael986 <juniornatan986@gmail.com>
2022-11-27 00:57:21 -0300

    fix layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import React from 'react';
-import Calculadora from './CALCULADORA/Calculadora';
 import Layout from './LAYOUT/Layout';
 
 function App() {
   return (
     <div>
       <Layout/>
-      <Calculadora/>
     </div>
   );
 }
--- src/LAYOUT/Layout.js
+++ src/LAYOUT/Layout.js
@@ -1,61 +1,58 @@
 import '../CSS/Styles.css';
+import './Layout.css'
 import React from 'react';
 
 class Layout extends React.Component {
     render() {
         return (
-            < div >
-                <nav class="navbar navbar-light bg-light fixed-top">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Offcanvas navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                            <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body">
-                                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Dropdown
-                                        </a>
-                                        <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-                                            <li><a class="dropdown-item" href="#">Action</a></li>
-                                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                                            <li>
-                                                <hr class="dropdown-divider" />
-                                            </li>
-                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <nav class="navbar navbar-light bg-light fixed-top">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">LAB. ENGENHARIA</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">ATIVIDADES</h5>
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+                                </li>
+                                <li class="mb-1">
+                                    <button class="Botao btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse1" aria-expanded="false">
+                                        1º Bimestre
+                                    </button>
+                                    <div class="collapse" id="home-collapse1">
+                                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                            <li><a href="#" class="link-dark rounded">Adivinhe o número</a></li>
+                                            <li><a href="#" class="link-dark rounded">Mostrar Data</a></li>
+                                            <li><a href="#" class="link-dark rounded">Hora/Tempo Real</a></li>
+                                            <li><a href="#" class="link-dark rounded">Palindromo</a></li>
+                                            <li><a href="#" class="link-dark rounded">Lista de Valores</a></li>
+                                            <li><a href="#" class="link-dark rounded">Animação de escrita</a></li>
                                         </ul>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Dropdown
-                                        </a>
-                                        <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-                                            <li><a class="dropdown-item" href="#">Action</a></li>
-                                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                                            <li>
-                                                <hr class="dropdown-divider" />
-                                            </li>
-                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </div>
+                                </li>
+                                <li class="mb-1">
+                                    <button class="Botao btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                                        2º Bimestre
+                                    </button>
+                                    <div class="collapse" id="home-collapse">
+                                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                            <li><a href="#" class="link-dark rounded">Overview</a></li>
+                                            <li><a href="#" class="link-dark rounded">Updates</a></li>
+                                            <li><a href="#" class="link-dark rounded">Reports</a></li>
                                         </ul>
-                                    </li>
-                                </ul>
-                            </div>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                </nav>
-            </div >
+                </div>
+            </nav>
         )
     }
 }
